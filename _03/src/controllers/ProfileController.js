a3d70a5bab6643612552036b338cb3c5fc544066
Raphael Gregg <raphaelgregg@gmail.com>
2021-04-07 01:26:59 -0300

    dia 03 commit

---
+++ _03/src/controllers/ProfileController.js
@@ -0,0 +1,30 @@
+const Profile = require("../model/Profile");
+
+module.exports = {
+  index(req, res) {
+    return res.render("profile", { profile: Profile.get() });
+  },
+  update(req, res) {
+    // req.body ara pegar dados
+    const data = req.body;
+    // definir quantas semanas tem um atribuindo
+    const weeksPerYear = 52;
+    // remover as semanas de ferias do ano, para pegar quantas semanas tem em 1 mes
+    const weeksPerMonth = (weeksPerYear - data["days-per-week"]) / 12;
+    // quantas horas por semana estou trabalhando
+    const weekTotalHours = data["hours-per-day"] * data["days-per-week"];
+    // total de horas trabalhadas no mes
+    const monthlyTotalHours = weekTotalHours * weeksPerMonth;
+    // qual valor da minha hora?
+    const hour = (data["value-hour"] =
+      data["monthly-budget"] / monthlyTotalHours);
+
+    Profile.update({
+      ...Profile.get(),
+      ...req.body,
+      valueHour: hour,
+    });
+
+    return res.redirect("/profile");
+  },
+};
